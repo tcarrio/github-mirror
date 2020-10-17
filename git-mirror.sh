fd857f04248746002313d2b90f2722bbe924f93b
Tom Carrio <tom@carrio.dev>
2020-10-17 18:12:09 -0400

    Simplying use cases to push all branches to remote

--- git-mirror.sh
+++ git-mirror.sh
@@ -34,7 +34,7 @@ pushd "${GIT_LOCAL_REPO}"
 
 set -x
 git remote add ${GIT_TARGET_REMOTE} ${GIT_TARGET_REPO}
-git branch --show-current | xargs git push --force ${GIT_TARGET_REMOTE}
+git push --all --force ${GIT_TARGET_REMOTE}
 
 set +x
 if [ $? -eq 0 ]
